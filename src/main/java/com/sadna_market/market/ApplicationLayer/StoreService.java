f3d3a28c17b2ae36b1349be12e138683f046da5f
Eden Bar <barede@post.bgu.ac.il>
2025-04-29 23:23:54 +0300

    fixed store service

--- src/main/java/com/sadna_market/market/ApplicationLayer/StoreService.java
+++ src/main/java/com/sadna_market/market/ApplicationLayer/StoreService.java
@@ -12,7 +12,6 @@ import com.sadna_market.market.DomainLayer.StoreExceptions.*;
 import com.sadna_market.market.InfrastructureLayer.RepositoryConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
